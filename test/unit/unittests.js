ce21d8e237f44cd274a6a023552570e94caccd59
Nathan Wilcox <nejucomo@gmail.com>
2013-10-09 19:48:47 -0700

    Add three tests for the new desired lafswapi interface which takes XHR via dependency injection.

--- test/unit/unittests.js
+++ test/unit/unittests.js
@@ -2,4 +2,24 @@ describe('lafs-wapi', function () {
   it('should be a window binding', function () {
     expect(window.lafswapi).toBeDefined();
   });
+
+  it('should be callable with no arguments', function () {
+    var client = lafswapi.Client();
+    expect(client).toBeDefined();
+  });
+
+  it('should be callable with a webapi URL', function () {
+    var url = 'https://public.wapi.example.com/lafs-v42.59/';
+    var client = lafswapi.Client(url);
+    expect(client).toBeDefined();
+    expect(client.url).toBe(url);
+  });
+
+  it('should be callable with a webapi URL and an XMLHttpRequest provider', function () {
+    var url = 'https://public.wapi.example.com/lafs-v42.59/';
+    var mockxhr = jasmine.Spy('XMLHttpRequest');
+    var client = lafswapi.Client(url, mockxhr);
+    expect(client).toBeDefined();
+    expect(client.url).toBe(url);
+  });
 });
