4494eb9bdf2ab2c0f8b3d22f25be7e5226a1e5d5
Nathan Wilcox <nejucomo@gmail.com>
2013-10-13 13:03:18 -0700

    Flesh out the BDD tests to the point of sending an XHR request for a get.

--- test/unit/unittests.js
+++ test/unit/unittests.js
@@ -1,20 +1,57 @@
+"use strict";
+
 describe('lafs-wapi', function () {
   it('should be a window binding', function () {
     expect(window.lafswapi).toBeDefined();
   });
 
-  it('should be callable with a webapi URL', function () {
-    var url = 'https://public.wapi.example.com/lafs-v42.59/';
-    var client = lafswapi.Client(url);
-    expect(client).toBeDefined();
-    expect(client.url).toBe(url);
-  });
+  describe('Client', function () {
+    it('should be callable with a webapi URL', function () {
+      var url = 'https://public.wapi.example.com/lafs-v42.59/';
+      var client = lafswapi.Client(url);
+      expect(client).toBeDefined();
+      expect(client.url).toBe(url);
+    });
 
-  it('should be callable with a webapi URL and an XMLHttpRequest provider', function () {
-    var url = 'https://public.wapi.example.com/lafs-v42.59/';
-    var mockxhr = jasmine.Spy('XMLHttpRequest');
-    var client = lafswapi.Client(url, mockxhr);
-    expect(client).toBeDefined();
-    expect(client.url).toBe(url);
+    it('should be callable with a webapi URL and an XMLHttpRequest provider', function () {
+      var url = 'https://public.wapi.example.com/lafs-v42.59/';
+      var mockxhr = jasmine.Spy('XMLHttpRequest');
+      var client = lafswapi.Client(url, mockxhr);
+      expect(client).toBeDefined();
+      expect(client.url).toBe(url);
+    });
+
+    describe('.get() method', function () {
+      it('should translate to an XMLHttpRequest request', function () {
+        var baseurl = 'https://public.wapi.example.com/lafs-v42.59/';
+        var cap = 'URI:FAKE_TEST_CAP:A';
+        var mockxhr = jasmine.createSpyObj(
+          'XMLHttpRequest',
+          ['open',
+           'send']);
+
+        var onreadystatechangevalue = undefined;
+        Object.defineProperty(
+          mockxhr, 'onreadystatechangevalue',
+          {
+            // FIXME: Use proper jasmine api for failure in get:
+            get: function () { throw new Error('Unexpected behavior.') },
+
+            set: function (v) {
+              expect(onreadystatechangevalue).not.toBeDefined();
+              onreadystatechangevalue = v;
+            },
+          });
+
+        var client = lafswapi.Client(baseurl, mockxhr);
+        client.get(cap, function (_) {});
+
+        expect(onreadystatechangevalue).toBeDefined();
+
+        var expectedurl = baseurl + '/uri/' + encodeURIComponent(cap);
+        expect(mockxhr.open).toHaveBeenCalledWith('GET', expectedurl);
+        expect(mockxhr.send).toHaveBeenCalledWith();
+      });
+    });
   });
 });
